4c9ddfdeac11768eabe97045d8c980ea779c6f58
Letandat071 <letandat3468@gmail.com>
2025-01-13 22:08:43 +0700

    Fix Vercel

--- src/components/wishlist/WishlistItem.tsx
+++ src/components/wishlist/WishlistItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
-import Button from '@/components/common/Button';
 
 interface WishlistItemProps {
   id: string;
